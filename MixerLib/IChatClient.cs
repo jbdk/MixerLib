ce6351f1391127179af4f3919b22a73a7c37dd54
jb <fezz.dk@gmail.com>
2018-04-08 11:09:36 +0200

    Updated help texts

--- MixerLib/IChatClient.cs
+++ MixerLib/IChatClient.cs
@@ -6,16 +6,31 @@ namespace MixerLib
 {
 	public interface IChatClient
 	{
+		/// <summary>True if the user is successfully authenticated against the endpoint</summary>
 		bool IsAuthenticated { get; }
 
+		/// <summary>Raised when a new chat/whisper message is received from the channel</summary>
 		event EventHandler<ChatMessageEventArgs> ChatMessage;
+
+		/// <summary>Raised when a user joins the channel</summary>
 		event EventHandler<ChatUserInfoEventArgs> UserJoined;
+
+		/// <summary>Raised when a user leaves the channel</summary>
 		event EventHandler<ChatUserInfoEventArgs> UserLeft;
 
+		/// <summary>Send a chat message</summary>
 		Task<bool> SendMessageAsync(string message);
+
+		/// <summary>Send a whisper message to a specific user</summary>
 		Task<bool> SendWhisperAsync(string userName, string message);
+
+		/// <summary>Timeout a user</summary>
 		Task<bool> TimeoutUserAsync(string userName, TimeSpan time);
+
+		/// <summary>Ban a user from the channel</summary>
 		Task<bool> BanUserAsync(string userName);
+
+		/// <summary>Unban a user from the channel</summary>
 		Task<bool> UnbanUserAsync(string userName);
 	}
 }
--- MixerLib/IMixerRestClient.cs
+++ MixerLib/IMixerRestClient.cs
@@ -6,12 +6,19 @@ namespace MixerLib
 {
 	public interface IMixerRestClient : IDisposable
 	{
+		/// <summary>True if a authorization token has been supplied to the library</summary>
 		bool HasToken { get; }
+
+		/// <summary>Name of the channel</summary>
 		string ChannelName { get; }
+
+		/// <summary>Id of the channel</summary>
 		uint? ChannelId { get; }
 
 		/// <summary>My user name or null if anonymously connected</summary>
 		string UserName { get; }
+
+		/// <summary>My user id or null if anonymously connected</summary>
 		uint? UserId { get; }
 
 		/// <summary>
@@ -22,14 +29,41 @@ namespace MixerLib
 		/// <returns>Current number of viewers and followers as a tuple</returns>
 		Task<(bool online, int viewers, int followers)> InitAsync(string channelName, string oauthToken);
 
+		/// <summary>Get chat endpoints and the chat websocket auth key</summary>
 		Task<API.Chats> GetChatAuthKeyAndEndpointsAsync();
+
+		/// <summary>Find the id of a user</summary>
 		Task<uint?> LookupUserIdAsync(string userName);
+
+		/// <summary>Ban a user from the channel</summary>
 		Task<bool> BanUserAsync(string userName);
+
+		/// <summary>Unban a user from the channel</summary>
 		Task<bool> UnbanUserAsync(string userName);
+
+		/// <summary>Get the time when the stream went online</summary>
+		/// <returns>Start time for stream, or null if offline</returns>
 		Task<DateTime?> GetStreamStartedAtAsync();
+
+		/// <summary>Search for a gametype</summary>
+		/// <returns>List of matching games (max 10)</returns>
 		Task<IEnumerable<API.GameTypeSimple>> LookupGameTypeAsync(string query);
+
+		/// <summary>Get game type with a give id</summary>
+		/// <returns>The game type, or null if not found</returns>
 		Task<API.GameTypeSimple> LookupGameTypeByIdAsync(uint gameTypeId);
+
+		/// <summary>
+		/// Set stream title and maybe game type
+		/// </summary>
+		/// <param name="title">Title of the stream</param>
+		/// <param name="gameTypeId">Game type id or null if it should not be changed</param>
 		Task UpdateChannelInfoAsync(string title, uint? gameTypeId = null);
+
+		/// <summary>
+		/// Get current stream title and gametype id
+		/// </summary>
+		/// <returns>Tuple with title and gametype id (might be null)</returns>
 		Task<(string title, uint? gameTypeId)> GetChannelInfoAsync();
 	}
 }
